790d79838cb911e224c722d7977f0ac6b7f5206f
anilsaini10 <anilsainik10@gmail.com>
2024-01-02 17:28:26 +0530

    changed endpoint

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@ const route = express.Router();
 
 const read_path = require("../controllers/read.js");
 
-route.get("/getList", read_path.allList);
-route.get("/", (req, res)=> {
-    return res.send("Hello")
-});
+route.get("/", read_path.allList);
+// route.get("/", (req, res)=> {
+//     return res.send("Hello")
+// });
 
 module.exports = route;
